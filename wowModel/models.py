537a0e251db5feb6a5f06ced4657f9c63797c038
孙淇 <sunqi@sunqideMacBook-Pro.local>
2020-12-06 22:17:17 +0800

    finish first part of models

--- wowModel/models.py
+++ wowModel/models.py
@@ -22,8 +22,53 @@ class Employee(models.Model):
     class Meta:  
         db_table = "employee"  
 
+class Veh_class(models.Model):
+    class_id = models.DecimalField(max_digits=4,decimal_places=0,primary_key=True)
+    class_name = models.CharField(max_length=32)
+    rental_rate = models.DecimalField(max_digits=10,decimal_places=2)
+    fees = models.DecimalField(max_digits=10,decimal_places=2)
+    class Meta:  
+        db_table = "class"  
+
+class Location(models.Model):
+    location_id = models.DecimalField(max_digits=4,decimal_places=0,primary_key=True)
+    country = models.CharField(max_length=32)
+    state = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
+    street = models.CharField(max_length=50)
+    zipcode = models.DecimalField(max_digits=5,decimal_places=0)
+    phone = models.DecimalField(max_digits=11, decimal_places=0)
+    class Meta:  
+        db_table = "location"  
+
+class Vehicle(models.Model):
+    vehicle_id = models.DecimalField(max_digits=8,decimal_places=0,primary_key=True)
+    make = models.CharField(max_length=32)
+    model = models.CharField(max_length=32)
+    year = models.DecimalField(max_digits=4,decimal_places=0)
+    vin = models.CharField(max_length=17)
+    lp_num = models.CharField(max_length=10)
+    class_id = models.ForeignKey(Veh_class, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default="")
+    class Meta:  
+        db_table = "vehicle"  
+
+class Rental_Record(models.Model):
+    record_id = models.DecimalField(max_digits=8,decimal_places=0,primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pickup_date =  models.DateField()
+    dropoff_date = models.DateField()
+    start_odo = models.DecimalField(max_digits=10,decimal_places=0)
+    end_odo = models.DecimalField(max_digits=10,decimal_places=0)
+    odo_limit = models.DecimalField(max_digits=10,decimal_places=0,null=True, blank=True)
+    vehicle = models.ForeignKey("Vehicle", on_delete=models.CASCADE)
+    pu_location = models.ForeignKey("Location", on_delete=models.CASCADE, related_name='pu_location_id')
+    do_location = models.ForeignKey("Location", on_delete=models.CASCADE, related_name='do_location_id')
+    class Meta:  
+        db_table = "rental_record"  
+
 class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     fname = models.CharField(max_length=32)
     lname = models.CharField(max_length=32)
     country = models.CharField(max_length=32)
@@ -31,6 +76,7 @@ class Customer(models.Model):
     city = models.CharField(max_length=32)
     street = models.CharField(max_length=50)
     zipcode = models.DecimalField(max_digits=5,decimal_places=0)
-    email = models.EmailField()
+    email = models.EmailField()     
     phone = models.DecimalField(max_digits=11, decimal_places=0)
     cust_type = models.CharField(max_length=1)
+
